26008258b84a84cae2905af93c23733767ce2b24
KIM WAN JUN <wanjun226@naver.com>
2018-07-19 21:07:28 +0900

    bug ..

--- buy_signal_agent/wanjun/main.py
+++ buy_signal_agent/wanjun/main.py
@@ -8,8 +8,9 @@ from gym_core import tgym
 import numpy as np
 
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten
+from keras.layers import Dense, Activation, Flatten, Input, Conv1D, Conv2D, MaxPooling1D, MaxPooling2D, Dropout, Concatenate
 from keras.optimizers import Adam
+from keras import models
 
 from rl.agents import DQNAgent
 from rl.memory import SequentialMemory
@@ -21,7 +22,6 @@ from rl.core import Processor
 from collections import deque
 from rl.callbacks import Callback
 
-import random
 EPISODES = 10000
 RENDER = False
 ACTION_SIZE = 2
@@ -65,9 +65,16 @@ class ObservationProcessor(Processor):
 
     def process_observation(self, observation):
         self.holder_observation.append(observation)
-        sell_hoga_price = int(observation[11])
-        buy_hoga_price = int(observation[31])
-        self.holder_info.append([sell_hoga_price, buy_hoga_price])
+        print('observation.shape : ' , observation.shape)
+        return np.array([100 * x for x in range(60)]), np.array([[-x for x in range(40)] for y in range(60)]), np.array([[0 for x in range(60)] for y in range(11)])
+
+        return np.array([[100 * x for x in range(60)], [[-x for x in range(40)] for y in range(60)], [[0 for x in range(60)] for y in range(11)]])
+        # input 1 - price history
+
+        # input 2 - order book history
+
+        # input 3 - transaction history
+
         tmp_obs = np.array([])
         for data in self.holder_observation:
             tmp_obs = np.concatenate((tmp_obs, data), axis=0)
@@ -92,24 +99,59 @@ class MyTGym(tgym.TradingGymEnv):
         else:
             return 0
 
+#
+# def build_network():
+#
+#     model = Sequential()
+#     model.add(Flatten(input_shape=(1,) + (3120,) )  )
+#     model.add(Dense(300))
+#     model.add(Activation('relu'))
+#     model.add(Dense(200))
+#     model.add(Activation('relu'))
+#     model.add(Dense(30))
+#     model.add(Activation('relu'))
+#     model.add(Dense(2))
+#     model.add(Activation('linear'))
+#     print(model.summary())
+#
+#     return model
+
 
 def build_network():
 
-    model = Sequential()
-    model.add(Flatten(input_shape=(1,) + (3120,) )  )
-    model.add(Dense(300))
-    model.add(Activation('relu'))
-    model.add(Dense(200))
-    model.add(Activation('relu'))
-    model.add(Dense(30))
-    model.add(Activation('relu'))
-    model.add(Dense(2))
-    model.add(Activation('linear'))
-    # print(model.summary())
-    print(model.output._keras_shape)
+    # price history
+    i1 = Input(shape=(60, 1))  # 60 features 1 channel
+    h1 = Conv1D(32, kernel_size=4, activation='relu')(i1)
+    h1 = Conv1D(64, kernel_size=4, activation='relu')(h1)
+    h1 = Flatten()(h1)
+
+    # order book history
+    i2 = Input(shape=(2, 20, 60))  # 2 x 20 features 60 channels
+    h2_1 = Conv2D(32, kernel_size=(2, 1), activation='relu')(i2)
+    h2_1 = Flatten()(h2_1)
+    h2_2 = Conv2D(32, kernel_size=(1, 2), activation='relu')(i2)
+    h2_2 = Flatten()(h2_2)
+    h2 = Concatenate()([h2_1, h2_2])
+
+    # transaction history
+    i3 = Input(shape=(60, 11))  # 60 features 11 channels
+    h3 = Conv1D(32, kernel_size=4, activation='relu')(i3)
+    h3 = Conv1D(64, kernel_size=4, activation='relu')(h3)
+    h3 = Flatten()(h3)
+
+    # concatenate
+    h = Concatenate()([h1, h2])
+    h = Concatenate()([h, h3])
+
+    h = Activation('relu')(Dense(64)(h))
+    h = Activation('relu')(Dense(24)(h))
+    y = Activation('linear')(Dense(2)(h))
+
+    model = models.Model([i1, i2, i3], y)
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.summary()
 
     return model
-
 
 if __name__ == '__main__':
 
@@ -118,30 +160,31 @@ if __name__ == '__main__':
 
     memory = SequentialMemory(limit=50000, window_length=1)
     policy = BoltzmannQPolicy()
-    model  = build_network()
+    model = build_network()
 
     logging.debug('dqn agent start..')
 
     model_path = 'save_model/{}_weights.h5f'.format('buy_signal_agent')
     chk_point = ModelIntervalCheckpoint(filepath=model_path, interval=50000)
-    obsprocesser = ObservationProcessor()
+    processor = ObservationProcessor()
 
     dqn = DQNAgent(model=model, nb_actions=2, memory=memory, nb_steps_warmup=60,
-                   target_model_update=1e-2, policy=policy, processor=obsprocesser)
+                   target_model_update=1e-2, policy=policy, processor=processor)
 
     dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 
-    #dqn.load_weights(model_path)
-    #dqn.load_weights('dqn_{}_weights.h5f'.format('trading'))
+    # dqn.load_weights(model_path)
+    # dqn.load_weights('dqn_{}_weights.h5f'.format('trading'))
 
     # Okay, now it's time to learn something! We visualize the training here for show, but this
     # slows down training quite a lot. You can always safely abort the training prematurely using
     # Ctrl + C.
-    dqn.fit(env, nb_steps=50000, visualize=False, verbose=2, callbacks=[chk_point])
+    while True:
+        dqn.fit(env, nb_steps=50000, visualize=False, verbose=2, callbacks=[chk_point])
 
-    # After training is done, we save the final weights.
-    dqn.save_weights(model_path, overwrite=True)
-    #dqn.save_weights('dqn_{}_weights.h5f'.format('trading'), overwrite=True)
+        # After training is done, we save the final weights.
+        dqn.save_weights(model_path, overwrite=True)
+        # dqn.save_weights('dqn_{}_weights.h5f'.format('trading'), overwrite=True)
 
-    # Finally, evaluate our algorithm for 5 episodes.
-    dqn.test(env, nb_episodes=5, visualize=True)
+        # Finally, evaluate our algorithm for 5 episodes.
+        dqn.test(env, nb_episodes=5, visualize=True)
