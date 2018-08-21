d34439de7f450efb741e9a9e2e8763bfd1961559
KIM WAN JUN <wanjun226@naver.com>
2018-08-22 01:01:19 +0900

    agent 적용 (미완)

--- aggregated_agent/main.py
+++ aggregated_agent/main.py
@@ -27,16 +27,16 @@ class DDQNAgent:
         self.batch_size = 32
         self.state_size = state_size
         self.action_size = action_size
-        self.train_start = 10000
+        self.train_start = 33
         self.target_update_interval = 10000
         self.memory = deque(maxlen=100000)
-        self.discount_factor = 0.9
+        self.discount_factor = 0.999
 
     def update_target_model(self):
         self.target_model.set_weights(self.model.get_weights())
 
     def get_action(self, state):
-        if np.random.rand() <= self.epsilon:
+        if np.random.random() <= self.epsilon:
             return random.randrange(self.action_size)
         else:
             q_value = self.model.predict(state)
@@ -47,7 +47,9 @@ class DDQNAgent:
 
     def train_model(self):
         if len(self.memory) < self.train_start:
+            print('memory size is to short', len(self.memory))
             return
+        print('train', len(self.memory))
 
         self.epsilon = max(self.epsilon * self.epsilon_decay, self.epsilon_min)
 
@@ -77,7 +79,9 @@ class DDQNAgent:
 
 
 class Agents:
+    agent_name = ['BSA', 'BOA', 'SSA', 'SOA']
     step_limit = [0, 30, 15, 0]
+    additional_reward_rate = 0.1
 
     def __init__(self, bsa, boa, ssa, soa):
         self.agents = [bsa, boa, ssa, soa]
@@ -130,28 +134,31 @@ class Agents:
         if self.sequence == 0:  # BSA
             pass
 
-        if self.sequence == 1:  # BOA
+        if self.sequence == 1 and not (self.boa_additional_data is None):  # BOA
             state = np.append(state, self.boa_additional_data)
 
-        if self.sequence == 2:  # SSA
+        if self.sequence == 2 and not (self.ssa_additional_data is None):  # SSA
             state = np.append(state, self.ssa_additional_data)
 
-        if self.sequence == 3:  # SOA
+        if self.sequence == 3 and not (self.soa_additional_data is None):  # SOA
             state = np.append(state, self.soa_additional_data)
 
         return state
 
     def append_sample(self, state, action, reward, next_state, done):
+        if self.sequence == 0:
+            print(reward[self.agent_name[self.sequence]])
         if action == 0:  # action 이 0 인 경우 additional reward 가 없으므로 그냥 memory 에 sample 추가
-            self.agents[self.sequence].append_sample(state, action, reward, next_state, done)
+            self.agents[self.sequence].append_sample(state, action, reward[self.agent_name[self.sequence]], next_state,
+                                                     done)
         else:  # action 이 1인 경우 additional reward 를 주기 위해 buffer 에 한번에 모았다가 reward 계산해서 마지막에 추가
-            self.sample_buffer.append([state, action, reward, next_state, done])
+            self.sample_buffer.append([state, action, reward[self.agent_name[self.sequence]], next_state, done])
         self._sequence_manage(action)
 
     def _append_buffer_sample(self):
-        self.sample_buffer[0][2] += self.sample_buffer[1][2] * 0.1 + self.sample_buffer[3][2] * 0.1
-        self.sample_buffer[1][2] += self.sample_buffer[3][2] * 0.1
-        self.sample_buffer[2][2] += self.sample_buffer[3][2] * 0.1
+        self.sample_buffer[0][2] += (self.sample_buffer[1][2] + self.sample_buffer[3][2]) * self.additional_reward_rate
+        self.sample_buffer[1][2] += self.sample_buffer[3][2] * self.additional_reward_rate
+        self.sample_buffer[2][2] += self.sample_buffer[3][2] * self.additional_reward_rate
         sb = self.sample_buffer
 
         for idx, agent in enumerate(self.agents):
@@ -167,9 +174,42 @@ class Agents:
 
 
 class MyTGym(tgym.TradingGymEnv):  # MyTGym 수정해야 함 -> agent 별 reward 를 줘야 함 (4개 반환해서 agents 가 수정하거나 agent 입력해서 reward 주거나)
-    def _rewards(self, observation, action, done, info):
-        return [0.1, 0, -0.4, 0.3][random.randint(0, 3)]
+    holder_observation = deque(np.array([[0 for x in range(52)] for y in range(60)]), maxlen=60)
 
+    def _rewards(self, observation, action, done, info):
+        rewards = {}
+        secs = 60
+
+        # create BSA reward
+        width = 0
+        price_at_signal = None
+        threshold = 0.33
+        for j in range(secs):
+            if j == 0:
+                price_at_signal = self.d_episodes_data[self.p_current_episode_ref_idx]['quote'].loc[
+                    self.c_range_timestamp[self.p_current_step_in_episode]]['Price(last excuted)']
+            else:
+                price = self.d_episodes_data[self.p_current_episode_ref_idx]['quote'].loc[self.c_range_timestamp[
+                    self.p_current_step_in_episode+j]]['Price(last excuted)']
+                gap = price - price_at_signal - threshold
+                width += gap
+        rewards['BSA'] = width
+
+        # create BOA rewrad
+        rewards['BOA'] = [0.1, 0, -0.4, 0.3][random.randint(0, 3)]
+
+        # create SSA reward
+        rewards['SSA'] = [0.1, 0, -0.4, 0.3][random.randint(0, 3)]
+
+        # create SOA reward
+        rewards['SOA'] = [0.1, 0, -0.4, 0.3][random.randint(0, 3)]
+        return rewards
+
+    def observation_processor(self, observation):
+        observation = {}
+
+        # create BSA observation
+        return np.array([0 for x in range(3120)])  # 임시 observation
 
 if __name__ == '__main__':
     env = MyTGym(episode_type='0', percent_goal_profit=2, percent_stop_loss=5, episode_duration_min=60)
@@ -187,7 +227,7 @@ if __name__ == '__main__':
 
         while not done:
             action = agents.get_action(state)
-            next_state, reward, done, info = env.step(action)  # reward 산정을 위해 어떤 agent 를 사용했는지 gym 에 알려줘야 할듯
+            next_state, reward, done, info = env.step(action)
             agents.append_sample(state, action, reward, next_state, done)
             state = next_state
             agents.train_agents()
