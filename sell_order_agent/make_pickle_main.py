ebf36492554e1e567ca8b9dee05b2d93d694ea59
iljoo.yoon <vpds@naver.com>
2018-08-26 13:05:46 +0900

    add elapse time
    add elapse time
    make pickle lighter

--- sell_order_agent/make_pickle_main.py
+++ sell_order_agent/make_pickle_main.py
@@ -13,7 +13,7 @@ import pickle
 import random
 
 def prepare_datasets(max_secs=120):
-    l = ioutil.load_data_from_dicrectory('0')
+    l = ioutil.load_data_from_directory('0')
     for li in l:
         prepare_dataset(li, max_secs)
 
@@ -28,47 +28,58 @@ def prepare_dataset(d, max_secs):
     # 시작 ~ 종료 까지 1초단위로 배열 생성
     c_rng_timestamp = pd.date_range(start=c_start, end=c_end, freq='S')
 
-    x_2d = []
-    x_1d = []
-    x_1d_lefttime = []
+    #x_2d = []
+    #x_1d = []
+    x_1d_second = []
+    x_1d_left_time = []
+    x_1d_elapsed_time = []
     y_1d = []
 
     for i, s in enumerate(c_rng_timestamp):
         # SSA 에서 보내주는 남은 시간 랜덤 생성.
         left_secs = random.randint(1, max_secs)
-        # BSA 에서 시그널을 보낸 후 경과한 시간.
+
+        # bsa_elapsed_secs : BSA 에서 시그널을 보낸 후 경과한 시간.
         bsa_elapsed_secs = max_secs - left_secs
-        # SSA 에서 시그널을 받고 실제 주문을 하는데 까지 걸린 시간을 랜덤 생성.
-        elapsed_secs = random.randint(0, left_secs)
-
-        # 장 시작보다 전에 시그널이 왔으면 skip
-        if i < bsa_elapsed_secs + elapsed_secs:
-            continue
-
-        # BSA 에서 시그널을 줄 수 있는 시간 이후에 시그널이 왔으면 skip
-        if len(c_rng_timestamp) - max_secs < i - bsa_elapsed_secs:
-            continue
 
         try:
             first_quote = d['quote'].loc[s]
             first_order = d['order'].loc[s]
 
-            # calculate Y
-            price_at_signal = d['quote'].loc[c_rng_timestamp[i - elapsed_secs]]['Price(last excuted)']
             price = d['quote'].loc[c_rng_timestamp[i]]['Price(last excuted)']
         except KeyError as e:
             print('찾을 수 없는 key 값이 있습니다.', current_ticker, e)
             continue
 
-        # SOA 가 파는 시점의 X, Y 데이터
-        x_1d_lefttime.append(left_secs)
-        x_2d.append(first_order)
-        x_1d.append(first_quote)
-        y_1d.append(price - price_at_signal)
+        # elapsed_secs : SSA 에서 시그널을 받고 경과한 시간
+        for elapsed_secs in (0, left_secs):
+            # 장 시작보다 전에 시그널이 왔으면 skip
+            if i < bsa_elapsed_secs + elapsed_secs:
+                continue
+
+            # BSA 에서 시그널을 줄 수 있는 시간 이후에 시그널이 왔으면 skip
+            if len(c_rng_timestamp) - max_secs < i - bsa_elapsed_secs:
+                continue
+
+            try:
+                price_at_signal = d['quote'].loc[c_rng_timestamp[i - elapsed_secs]]['Price(last excuted)']
+            except KeyError as e:
+                print('찾을 수 없는 key 값이 있습니다.', current_ticker, e)
+                break
+
+            # SOA 가 파는 시점의 X, Y 데이터
+            # 시그널 받은 시점의 남은 시간
+            x_1d_left_time.append(left_secs)
+            # 시그널을 받고 나서 경과한 시간
+            x_1d_elapsed_time.append(elapsed_secs)
+            x_1d_second(s)
+            #x_2d.append(first_order)
+            #x_1d.append(first_quote)
+            y_1d.append(price - price_at_signal)
 
     pickle_name = current_date + '_' + current_ticker + '.pickle'
     f = open('./pickles/'+pickle_name, 'wb')
-    pickle.dump([x_2d, x_1d, x_1d_lefttime, y_1d], f)
+    pickle.dump([x_1d_second, x_1d_left_time, x_1d_elapsed_time, y_1d], f)
     f.close()
 
 prepare_datasets()
