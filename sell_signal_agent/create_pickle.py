f17342726a2418e794697f04a7bcecab1385f38c
doni <doni lee>
2018-09-09 06:03:26 +0900

    ssa

---
+++ sell_signal_agent/create_pickle.py
@@ -0,0 +1,192 @@
+from gym_core import ioutil  # file i/o to load stock csv files
+from collections import deque
+import config
+import pandas as pd
+import datetime
+import numpy as np
+import pickle
+import os
+import random
+
+"""
+previously,  I gave secs as 120. but like iljoo said, it needs to be 120.
+in other agents, it can changes but you don't have to read every time seconds periods changes
+just read maximum periods of data and reuse it   
+"""
+def prepare_datasets(is_spare_dataset=False, interval=120, len_observation=60, len_sequence_secs=120, save_dir=''):
+    """
+    main coordinate fucntion to create pickle
+    :param is_spare_dataset: if true, it uses not prepare_dataset function, but prepares_spare_dataset function.
+    :param interval: same as prepare_sparse_dataset
+    :param len_observation: only used if is_spare_dataset is true
+    :param len_sequence_secs: same as prepare_sparse_dataset
+    :param save_dir: root directory where pickle will save
+    :return:
+    """
+    # l = ioutil.load_data_from_directory('0', max_n_episode=1) # episode type
+    l = ioutil.load_data_from_directory('0')  # episode type
+    for li in l:
+        if is_spare_dataset:
+            prepare_sparse_dataset(li, 120, 120, 60, save_dir)
+        else:
+            prepare_dataset(li, 1, len_sequence_secs)
+
+
+def prepare_sparse_dataset(d, interval=120, len_sequence_of_secs=120, len_observation=60, save_dir=''):
+    """
+    original version
+    loading data from ticker 20180403, yyyymmdd 003350 is started.
+    executed time :  1538.569629558868 -> 25.6 minutes!! ( each episode would be 100 mb)
+    This function is to get more sparse data set. It is created to make loading time from pickle into memory fast
+    :param d:  same as prepare_dataset
+    :param interval: same as prepare_dataset, 120 seconds. it is also for performance
+    :param len_sequence_of_secs:  same as prepare_dataset.
+    :param len_observation: Instead of 120 seconds, taking 60 seconds is just for performance
+    :param save_dir: root directory where pickle will save
+    :return: same as prepare_dataset
+    """
+    current_date = d['meta']['date']
+    current_ticker = d['meta']['ticker']
+
+    c_start = datetime.datetime(int(current_date[0:4]), int(current_date[4:6]),
+                                int(current_date[6:8]), 9, 5)  # 9hr 5min 0sec, start time
+    c_end = datetime.datetime(int(current_date[0:4]), int(current_date[4:6]),
+                              int(current_date[6:8]), 15, 20)  # 15hr 20min 0sec, finish time
+    c_rng_ts = pd.date_range(start=c_start, end=c_end,
+                                    freq=str(interval)+'S')  # range between c_start and c_end saving each seconds' data
+
+    max_idx = len(c_rng_ts) - 1
+
+    x_2d = []  # orderbook
+    x_1d = []  # transactions
+    y_1d = []  # width
+
+    # 경과시간
+    elapsed_time = []
+    # 남은시간
+    left_time = []
+
+    for i, s in enumerate(c_rng_ts):
+        # BOA 에서 보내주는 남은 시간 랜덤 생성.
+        left_secs = random.randint(1, max_secs)
+
+        # SSA 에서 시그널 발생 하는데 까지 걸린 시간을 랜덤 생성.
+        elapsed_secs = random.randint(0, left_secs)
+
+        d_x2d = deque(maxlen=len_observation)
+        d_x1d = deque(maxlen=len_observation)
+
+        if c_rng_ts[max_idx] < s + pd.Timedelta(seconds=len_sequence_of_secs) or i >= max_idx:
+            break
+        elif s - pd.Timedelta(seconds=len_observation) < c_rng_ts[0]:
+            continue
+        else:
+            # first_quote = d['quote'].loc[s]
+            # first_order = d['order'].loc[s]
+            width = 0
+            threshold = 0.33
+
+            # assemble observation for len_observation
+            for i in reversed(range(len_observation)):
+                d_x2d.append(d['order'].loc[s-pd.Timedelta(seconds=i)])
+                d_x1d.append(d['quote'].loc[s-pd.Timedelta(seconds=i)])
+
+            # price_at_signal is the price when the current stock received signal
+            price_at_signal = d['quote'].loc[c_rng_ts[i-elapsed_secs]]['Price(last excuted)']
+            price = d['quote'].loc[c_rng_ts[i]]['Price(last excuted)']
+            gap = price_at_signal - price - threshold
+            width += gap
+
+            x_2d.append(np.array(d_x2d))
+            x_1d.append(np.array(d_x1d))
+            left_time.append(left_secs)
+            elapsed_time.append(elapsed_secs)
+            y_1d.append(width)
+
+    pickle_name = save_dir + os.path.sep + current_date + '_' + current_ticker + '.pickle'
+    f = open(pickle_name, 'wb')
+    pickle.dump([x_2d, x_1d, elapsed_time, left_time, y_1d], f)
+    f.close()
+
+
+def prepare_dataset(d, interval=1, len_sequence_of_secs=120):
+    """
+    :param d
+        the variable having pickle file data in memory
+    :param interval
+        a period between previous observation and current observation,
+        if it bring data moving 1 second forward, data size is so huge.
+    :param len_sequence_of_secs:
+        each observation length
+    :return:
+        nothing, it end up saving list of pickle files as you configured
+    """
+    current_date = d['meta']['date']
+    current_ticker = d['meta']['ticker']
+
+    d_price = deque(maxlen=len_sequence_of_secs)
+
+    c_start = datetime.datetime(int(current_date[0:4]), int(current_date[4:6]),
+                                int(current_date[6:8]), 9, 5)  # 9hr 5min 0sec, start time
+    c_end = datetime.datetime(int(current_date[0:4]), int(current_date[4:6]),
+                              int(current_date[6:8]), 15, 20)  # 15hr 20min 0sec, finish time
+    c_rng_ts = pd.date_range(start=c_start, end=c_end,
+                                    freq=str(interval)+'S')  # range between c_start and c_end saving each seconds' data
+
+    x_2d = []  # orderbook
+    x_1d = []  # transactions
+    y_1d = []  # width
+
+    # 경과시간
+    elapsed_time = []
+    # 남은시간
+    left_time = []
+
+    max_idx = len(c_rng_ts) - 1
+
+    for i, s in enumerate(c_rng_ts):
+        # BOA 에서 보내주는 남은 시간 랜덤 생성.
+        left_secs = random.randint(1, max_secs)
+        # BSA 에서 시그널을 보낸 후 경과한 시간.
+        bsa_elapsed_secs = max_secs - left_secs
+        # SSA 에서 시그널 발생 하는데 까지 걸린 시간을 랜덤 생성.
+        elapsed_secs = random.randint(0, left_secs)
+
+        if c_rng_ts[max_idx] < c_rng_ts[i] + len_sequence_of_secs or i >= max_idx:
+            break
+        elif s - len_sequence_of_secs < c_rng_ts[0]:
+            continue
+        else:
+            first_quote = d['quote'].loc[s]
+            first_order = d['order'].loc[s]
+            width = 0
+            threshold = 0.33
+
+            # calculate width
+            for j in range(len_sequence_of_secs):
+                if j == 0:
+                    # price_at_signal is the price when the current stock received signal
+                    price_at_signal = d['quote'].loc[c_rng_ts[i+j]]['Price(last excuted)']
+                else:
+                    price = d['quote'].loc[c_rng_ts[i+j]]['Price(last excuted)']
+                    gap = price_at_signal - price - threshold
+                    width += gap
+
+            left_time.append(left_secs)
+            elapsed_time.append(elapsed_secs)
+            x_2d.append(first_order)
+            x_1d.append(first_quote)
+            y_1d.append(width)
+
+    pickle_name = current_date + '_' + current_ticker + '.pickle'
+    f = open(pickle_name, 'wb')
+    pickle.dump([x_2d, x_1d, y_1d], f)
+    f.close()
+
+
+save_dir = 'sparse'
+max_secs = 120
+if not os.path.isdir(save_dir):
+    os.makedirs(save_dir)
+
+prepare_datasets(is_spare_dataset=True, save_dir=save_dir)
---
+++ sell_signal_agent/train.py
@@ -0,0 +1,376 @@
+from keras.models import Model
+from keras.layers import Input, Dense, Conv3D, Conv1D, Dense, Flatten, MaxPooling1D, MaxPooling2D,MaxPooling3D,Concatenate
+import numpy as np
+import pickle
+from rl.callbacks import FileLogger, ModelIntervalCheckpoint
+from gym_core.ioutil import *  # file i/o to load stock csv files
+import logging
+
+
+"""
+it will prevent process not to occupying 100% of gpu memory for the first time. 
+Instead, it will use memory incrementally.
+"""
+# import tensorflow as tf
+# from keras.backend.tensorflow_backend import set_session
+# config = tf.ConfigProto()
+# config.gpu_options.per_process_gpu_memory_fraction = 0.2
+# set_session(tf.Session(config=config))
+
+"""
+build q newtork using cnn and dense layer
+"""
+def build_network():
+    input_order = Input(shape=(10, 2, 120, 2), name="x1")
+    input_tranx = Input(shape=(120, 11), name="x2")
+
+    h_conv1d_2 = Conv1D(filters=16, kernel_size=3, activation='relu')(input_tranx)
+    h_conv1d_4 = MaxPooling1D(pool_size=3, strides=None, padding='valid')(h_conv1d_2)
+    h_conv1d_6 = Conv1D(filters=32, kernel_size=3, activation='relu')(h_conv1d_4)
+    h_conv1d_8 = MaxPooling1D(pool_size=2, strides=None, padding='valid')(h_conv1d_6)
+
+    h_conv3d_1_1 = Conv3D(filters=16, kernel_size=(2, 1, 5), activation='relu')(input_order)
+    h_conv3d_1_2 = Conv3D(filters=16, kernel_size=(1, 2, 5), activation='relu')(input_order)
+
+    h_conv3d_1_3 = MaxPooling3D(pool_size=(1, 1, 3))(h_conv3d_1_1)
+    h_conv3d_1_4 = MaxPooling3D(pool_size=(1, 1, 3))(h_conv3d_1_2)
+
+    h_conv3d_1_5 = Conv3D(filters=32, kernel_size=(1, 2, 5), activation='relu')(h_conv3d_1_3)
+    h_conv3d_1_6 = Conv3D(filters=32, kernel_size=(2, 1, 5), activation='relu')(h_conv3d_1_4)
+
+    h_conv3d_1_7 = MaxPooling3D(pool_size=(1, 1, 5))(h_conv3d_1_5)
+    h_conv3d_1_8 = MaxPooling3D(pool_size=(1, 1, 5))(h_conv3d_1_6)
+    o_conv3d_1 = Concatenate(axis=-1)([h_conv3d_1_7, h_conv3d_1_8])
+
+    o_conv3d_1_1 = Flatten()(o_conv3d_1)
+
+    i_concatenated_all_h_1 = Flatten()(h_conv1d_8)
+
+    i_concatenated_all_h = Concatenate()([i_concatenated_all_h_1, o_conv3d_1_1])
+
+    output = Dense(1, activation='linear')(i_concatenated_all_h)
+
+    model = Model([input_order, input_tranx], output)
+
+    return model
+
+
+"""
+build q newtork using cnn and dense layer
+"""
+def build_network_for_sparsed():
+    input_order = Input(shape=(10, 2, 60, 2), name="x1")
+    input_tranx = Input(shape=(60, 11), name="x2")
+    input_elapedtime = Input(shape=(7,), name="x3")
+    input_lefttime = Input(shape=(7,), name="x4")
+
+    h_conv1d_2 = Conv1D(filters=16, kernel_size=3, activation='relu')(input_tranx)
+    h_conv1d_4 = MaxPooling1D(pool_size=3, strides=None, padding='valid')(h_conv1d_2)
+    h_conv1d_6 = Conv1D(filters=32, kernel_size=3, activation='relu')(h_conv1d_4)
+    h_conv1d_8 = MaxPooling1D(pool_size=2, strides=None, padding='valid')(h_conv1d_6)
+
+    h_conv3d_1_1 = Conv3D(filters=16, kernel_size=(2, 1, 5), activation='relu')(input_order)
+    h_conv3d_1_2 = Conv3D(filters=16, kernel_size=(1, 2, 5), activation='relu')(input_order)
+
+    h_conv3d_1_3 = MaxPooling3D(pool_size=(1, 1, 3))(h_conv3d_1_1)
+    h_conv3d_1_4 = MaxPooling3D(pool_size=(1, 1, 3))(h_conv3d_1_2)
+
+    h_conv3d_1_5 = Conv3D(filters=32, kernel_size=(1, 2, 5), activation='relu')(h_conv3d_1_3)
+    h_conv3d_1_6 = Conv3D(filters=32, kernel_size=(2, 1, 5), activation='relu')(h_conv3d_1_4)
+
+    h_conv3d_1_7 = MaxPooling3D(pool_size=(1, 1, 5))(h_conv3d_1_5)
+    h_conv3d_1_8 = MaxPooling3D(pool_size=(1, 1, 5))(h_conv3d_1_6)
+    o_conv3d_1 = Concatenate(axis=-1)([h_conv3d_1_7, h_conv3d_1_8])
+
+    o_conv3d_1_1 = Flatten()(o_conv3d_1)
+
+    i_concatenated_all_h_1 = Flatten()(h_conv1d_8)
+
+    i_concatenated_all_h = Concatenate()([i_concatenated_all_h_1, o_conv3d_1_1, input_elapedtime, input_lefttime])
+
+    output = Dense(1, activation='linear')(i_concatenated_all_h)
+
+    model = Model([input_order, input_tranx, input_elapedtime, input_lefttime], output)
+
+    return model
+
+
+
+
+
+def get_sample_data(count):
+
+    start = 0
+    ld_x1 = []
+    ld_x2 = []
+    ld_y = []
+    d1 = []
+
+    # x1_dimension_info = (10, 2, 120, 2)  # 60 --> 120 (@ilzoo)
+    # x2_dimension_info = (120, 11)
+    # y1_dimension_info = (120,)
+
+    for i in range(count):
+        d1_2 = np.arange(start, start + 2 * 10 * 120 * 2).reshape([10, 2, 120, 2])
+        start += 2 * 10 * 120
+        d2 = np.arange(start, start+11*120).reshape([120, 11])
+        start += 11 * 120
+        ld_x1.append(d1_2)
+
+        # ld_x1.append([d1_1, d1_2])
+        ld_x2.append(d2)
+
+    for j in range(count):
+        d1 = np.arange(start, start + 1)
+        ld_y.append(d1)
+
+    return np.asarray(ld_x1), np.asarray(ld_x2), np.asarray(ld_y)
+
+
+
+
+def get_real_data_sparsed(ticker='001470', date='20180420', train_data_rows=None, save_dir=''):
+    """
+    Get sparsed data for supervised learning
+    :param ticker: ticker number to read
+    :param date: date yyyymmdd to read
+    :param train_data_rows: data rows to read for training, default None : read all rows
+    :param save_dir: default ''
+    :return: training data x1, x2, y1 for supervised learning model
+    """
+    current_ticker = ticker
+    current_date = date
+
+    x1_dimension_info = (10, 2, 60, 2)  # 60 --> 120 (@iljoo)
+    x2_dimension_info = (60, 11)
+    x3_dimension_info = (7,)
+    x4_dimension_info = (7,)
+    # y1_dimension_info = (120,)
+
+    pickle_name = save_dir + os.path.sep + current_ticker + '_' + current_date + '.pickle'
+    f = open(pickle_name, 'rb')
+    d = pickle.load(f)  # d[data_type][second] : mapobject!!
+    f.close()
+
+    total_rows = len(d[0])
+
+    if train_data_rows is None:
+        train_data_rows = len(d[0])
+
+    x1 = np.zeros([10, 2, 60, 2])
+    x2 = np.zeros([60, 11])
+    x3 = np.zeros([7,])
+    x4 = np.zeros([7,])
+    y1 = np.zeros([120])
+
+    d_x1 = []
+    d_x2 = []
+    d_x3 = []
+    d_x4 = []
+    d_y1 = []
+
+    for idx in range(train_data_rows):
+        for row in range(x1_dimension_info[0]):  # 10 : row
+            for column in range(x1_dimension_info[1]):  # 2 : column
+                for second in range(x1_dimension_info[2]):  # 60: seconds
+                    for channel in range(x1_dimension_info[3]):  # 2 : channel
+                        x1[row][column][second][channel] = d[0][idx][second][channel*20+column*10]
+        d_x1.append(x1)
+
+        for second in range(x2_dimension_info[0]):  # 120 : seconds
+            for feature in range(x2_dimension_info[1]):  # 11 : features
+                x2[second, feature] = d[1][idx][second][feature]
+        d_x2.append(x2)
+
+        for second in range(x3_dimension_info[0]):
+            x3[second] = d[2][idx]
+        d_x3.append(x3)
+
+        for second in range(x4_dimension_info[0]):
+            x3[second] = d[3][idx]
+        d_x4.append(x4)
+
+        # for second in range(y1_dimension_info[0]): # 60 : seconds
+        d_y1.append(d[2][idx])
+
+    return np.asarray(d_x1), np.asarray(d_x2), np.asarray(d_x3), np.asarray(d_x4), np.asarray(d_y1)
+
+
+def get_real_data(ticker='001470', date='20180420', train_data_rows=None, save_dir=''):
+    """
+    Get data for supervised learning
+    :param ticker: ticker number to read
+    :param date: date yyyymmdd to read
+    :param train_data_rows: data rows to read for training, default None : read all rows
+    :param save_dir: default ''
+    :return: training data x1, x2, y1 for supervised learning model
+    """
+    current_ticker = ticker
+    current_date = date
+
+    x1_dimension_info = (10, 2, 120, 2)  # 60 --> 120 (@iljoo)
+    x2_dimension_info = (120, 11)
+    # y1_dimension_info = (120,)
+
+    pickle_name = save_dir + os.path.sep + current_ticker + '_' + current_date + '.pickle'
+    f = open(pickle_name, 'rb')
+    d = pickle.load(f)  # d[data_type][second] : mapobject!!
+    f.close()
+
+    total_rows = len(d[0])
+
+    if train_data_rows is None:
+        train_data_rows = len(d[0])
+
+    x1 = np.zeros([10, 2, 120, 2])
+    x2 = np.zeros([120, 11])
+    y1 = np.zeros([120])
+
+    d_x1 = []
+    d_x2 = []
+    d_y1 = []
+
+    for idx_second in range(train_data_rows):
+
+        if idx_second + 120 > total_rows:
+            break
+
+        for row in range(x1_dimension_info[0]):  # 10 : row
+            for column in range(x1_dimension_info[1]):  # 2 : column
+                for second in range(x1_dimension_info[2]):  # 120 : seconds
+                    for channel in range(x1_dimension_info[3]):  # 2 : channel
+                        key = ''
+                        if channel == 1:
+                            key = 'Buy'
+                        else:
+                            key = 'Sell'
+                        if column == 0:
+                            value = 'Hoga'
+                        else:
+                            value = 'Order'
+
+                        x1[row][column][second][channel] = d[0][idx_second+second][key+value+str(row+1)]
+        d_x1.append(x1)
+
+        for second in range(x2_dimension_info[0]):  # 120 : seconds
+            for feature in range(x2_dimension_info[1]):  # 11 : features
+                x2[second, feature] = d[1][idx_second+second][feature]
+        d_x2.append(x2)
+
+        # for second in range(y1_dimension_info[0]): # 60 : seconds
+        d_y1.append(d[2][idx_second])
+
+    return np.asarray(d_x1), np.asarray(d_x2), np.asarray(d_y1)
+
+# def train_using_fake_data():
+#     train_per_each_episode('','',True)
+
+
+def train_using_real_data(d, save_dir=''):
+
+    model = build_network()
+    model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
+    model.summary()
+
+    l = load_ticker_yyyymmdd_list_from_directory(d)
+
+    t_x1, t_x2, t_y1 = [], [], []
+
+    for (ti, da) in l:
+        print('loading data from ticker {}, yyyymmdd {} is started.'.format(ti, da))
+        x1, x2, y1 = load_data(ti, da, use_fake_data=False, save_dir=save_dir)
+        t_x1.append(x1)
+        t_x2.append(x2)
+        t_y1.append(y1)
+        print('loading data from ticker {}, yyyymmdd {} is finished.'.format(ti, da))
+
+    print('total x1 : {}, total x2 : {}, total y1 : {}'.format(len(t_x1), len(t_x2), len(t_y1)))
+
+    # {steps} --> this file will be saved whenever it runs every steps as much as {step}
+    checkpoint_weights_filename = 'bsa_' + 'fill_params_information_in_here' + '_weights_{step}.h5f'
+
+    # TODO: here we can add hyperparameters information like below!!
+    log_filename = 'bsa_{}_log.json'.format('fill_params_information_in_here')
+    checkpoint_interval = 50
+
+    callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename, interval=checkpoint_interval)]
+    callbacks += [FileLogger(log_filename, interval=100)]
+
+    print('start to train.')
+    model.fit({'x1': t_x1, 'x2': t_x2}, t_y1, epochs=10, verbose=2, batch_size=64, callbacks=callbacks)
+    model.save_weights('final_weight.h5f')
+
+def train_using_real_data_sparsed(d, save_dir=''):
+
+    model = build_network_for_sparsed()
+    model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
+    # model.summary()
+
+    l = load_ticker_yyyymmdd_list_from_directory(d)
+
+    t_x1, t_x2, t_x3, t_x4, t_y1 = [],[],[],[],[]
+
+    for (ti, da) in l:
+        print('loading data from ticker {}, yyyymmdd {} is started.'.format(ti, da))
+        x1, x2, x3, x4, y1 = load_data_sparsed(ti, da, use_fake_data=False, save_dir=save_dir)
+        t_x1.append(x1)
+        t_x2.append(x2)
+        t_x3.append(x3)
+        t_x4.append(x4)
+        t_y1.append(y1)
+        print('loading data from ticker {}, yyyymmdd {} is finished.'.format(ti, da))
+    t_x1 = np.concatenate(t_x1)
+    t_x2 = np.concatenate(t_x2)
+    t_x3 = np.concatenate(t_x3)
+    t_x4 = np.concatenate(t_x4)
+
+    t_y1 = np.concatenate(t_y1)
+
+    print('total x1 : {}, total x2 : {}, total y1 : {}'.format(len(t_x1), len(t_x2), len(t_y1)))
+    # np.append(t_x1,values=(36,8,10,2,60))
+
+    # {steps} --> this file will be saved whenever it runs every steps as much as {step}
+    checkpoint_weights_filename = 'ssa_' + 'fill_params_information_in_here' + '_weights_{step}.h5f'
+
+    # TODO: here we can add hyperparameters information like below!!
+    log_filename = 'ssa_{}_log.json'.format('fill_params_information_in_here')
+    checkpoint_interval = 50
+
+    callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename, interval=checkpoint_interval)]
+    callbacks += [FileLogger(log_filename, interval=100)]
+
+    print('start to train.')
+    model.fit({'x1': t_x1, 'x2': t_x2, 'x3': t_x3, 'x4': t_x4}, t_y1, epochs=50, verbose=2, batch_size=64, callbacks=callbacks)
+    model.save_weights('final_weight.h5f')
+
+
+
+
+def load_data(t, d, use_fake_data=False, save_dir =''):
+    if use_fake_data:
+        x1, x2, y = get_sample_data(10)
+    else:
+        current_date = d
+        current_ticker = t
+        #if you give second as None, it will read every seconds in file.
+        # x1, x2, y = get_real_data(current_ticker, current_date, train_data_rows=130)
+        x1, x2, y = get_real_data(current_ticker, current_date, save_dir=save_dir)
+    return x1, x2, y
+
+
+
+def load_data_sparsed(t, d, use_fake_data=False, save_dir =''):
+    if use_fake_data:
+        x1, x2, x3, x4, y = get_sample_data(10)
+    else:
+        current_date = d
+        current_ticker = t
+        #if you give second as None, it will read every seconds in file.
+        # x1, x2, y = get_real_data(current_ticker, current_date, train_data_rows=130)
+        x1, x2, x3, x4, y = get_real_data_sparsed(current_ticker, current_date, save_dir=save_dir)
+    return x1, x2, x3, x4, y
+
+# train_using_fake_data()
+d  = os.path.abspath(os.path.dirname(__file__)) + "\\sparse"
+# train_using_real_data(d, 'sparse')
+train_using_real_data_sparsed(d, 'sparse')
