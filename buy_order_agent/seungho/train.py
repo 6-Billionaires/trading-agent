09f45ee27c403892aaca2ea8f92cf9aa454f5bfd
Seungho <onmylife82@gmail.com>
2018-09-11 22:04:34 +0900

    grid add

--- buy_order_agent/seungho/train.py
+++ buy_order_agent/seungho/train.py
@@ -8,13 +8,15 @@ from rl.callbacks import FileLogger, ModelIntervalCheckpoint
 from gym_core import ioutil  # file i/o to load stock csv files
 import random
 from core import util
+from core.scikit_learn_multi_input import KerasRegressor
+from sklearn.model_selection import GridSearchCV
 
 """
 build q newtork using cnn and dense layer
 """
 
 
-def build_network(max_secs, max_len):
+def build_network(max_secs = 90, max_len = 7):
     """
     here, seungho!!
 
@@ -78,70 +80,65 @@ def get_real_data(max_secs, max_len, csv, pickles, max_stock = 5):
     keys = list(pickles.keys())
 
     random.shuffle(keys)
-    # print('------', str_episode, end_episode)
-    for stock in range(1, max_stock):
 
+    max_stock = max_stock if max_stock < len(keys) else len(keys)
+    stock = 0
+    for key in keys:
+        stock = stock + 1
+        if stock > max_stock:
+            break
         sys.stdout.write("\r%i" % stock + " / %i 완료"  %max_stock)
         sys.stdout.flush()
-        for key in keys:
-            # print('------1', episode, key)
-            if len(pickles) < stock:
-                continue
 
-            # index (second) : second - 120 + i
-            #pickles[key][idx][0] - 120
-            # left_secs : 120 초간 동일
-            #pickles[key][idx][1]
-            # elapsed_secs : max(elapsed_secs - 120 + i, 0)
-            #pickles[key][idx][2]
-            # y
-            #pickles[key][idx][3]
+        # print('------1', episode, key)
+        if len(pickles) < stock:
+            continue
 
-            start_sec = 0
-            # print('++++', key, episode, pickles[key][0], len(csv[key]['order']))
-            x1 = np.zeros([10, 2, max_secs, 2])
-            for second in range(x1_dimension_info[2]):  # 90 : seconds
+        start_sec = 0
+        # print('++++', key, episode, pickles[key][0], len(csv[key]['order']))
+        x1 = np.zeros([10, 2, max_secs, 2])
+        for second in range(x1_dimension_info[2]):  # 90 : seconds
 
-                tmp = csv[key]['order'].loc[pickles[key][0][start_sec]+second]
-                # print('------2', second)
-                for row in range(x1_dimension_info[0]):  #10 : row
-                    for column in range(x1_dimension_info[1]):  #2 : column
-                        for channel in range(x1_dimension_info[3]):  #2 : channel
-                            buy_sell = ''
-                            if channel == 1:
-                                buy_sell = 'Buy'
-                            else:
-                                buy_sell = 'Sell'
+            tmp = csv[key]['order'].loc[pickles[key][0][start_sec]+second]
+            # print('------2', second)
+            for row in range(x1_dimension_info[0]):  #10 : row
+                for column in range(x1_dimension_info[1]):  #2 : column
+                    for channel in range(x1_dimension_info[3]):  #2 : channel
+                        buy_sell = ''
+                        if channel == 1:
+                            buy_sell = 'Buy'
+                        else:
+                            buy_sell = 'Sell'
 
-                            if column == 0:
-                                value = 'Hoga'
-                            else:
-                                value = 'Order'
+                        if column == 0:
+                            value = 'Hoga'
+                        else:
+                            value = 'Order'
 
-                            x1[row][column][second][channel] = tmp[buy_sell+value+str(row+1)]
-            d_x1.append(x1)
+                        x1[row][column][second][channel] = tmp[buy_sell+value+str(row+1)]
+        d_x1.append(x1)
 
-            x2 = np.zeros([max_secs, 11])
-            for second in range(x2_dimension_info[0]):  #120 : seconds
-                tmp = csv[key]['quote'].loc[pickles[key][0][start_sec]+second]
-                for feature in range(x2_dimension_info[1]):  #11 :features
-                    x2[second, feature] = tmp[feature]
-            d_x2.append(x2)
+        x2 = np.zeros([max_secs, 11])
+        for second in range(x2_dimension_info[0]):  #120 : seconds
+            tmp = csv[key]['quote'].loc[pickles[key][0][start_sec]+second]
+            for feature in range(x2_dimension_info[1]):  #11 :features
+                x2[second, feature] = tmp[feature]
+        d_x2.append(x2)
 
-            x3 = np.zeros([max_len])
-            binary_second = util.seconds_to_binary_array(max_secs, max_len)
-            for feature in range(x3_dimension_info[0]):  # max_len :features
-                x3[feature] = binary_second[feature]
+        x3 = np.zeros([max_len])
+        binary_second = util.seconds_to_binary_array(max_secs, max_len)
+        for feature in range(x3_dimension_info[0]):  # max_len :features
+            x3[feature] = binary_second[feature]
 
-            d_x3.append(x3)
+        d_x3.append(x3)
 
-            # for second in range(y1_dimension_info[0]): #60 : seconds
-            d_y1.append(pickles[key][2][start_sec])
+        # for second in range(y1_dimension_info[0]): #60 : seconds
+        d_y1.append(pickles[key][2][start_sec])
 
     sys.stdout.write("\r")
     sys.stdout.flush()
 
-    return np.asarray(d_x1), np.asarray(d_x2), np.asarray(d_y1)
+    return np.asarray(d_x1), np.asarray(d_x2), np.asarray(d_x3), np.asarray(d_y1)
 
 
 def train_using_real_data(data, max_secs, max_len):
@@ -150,44 +147,62 @@ def train_using_real_data(data, max_secs, max_len):
     # {일자 + 종목코드} : [second, left_time, elapsed_time, y]
     pickles, max_size = ioutil.load_ticker_yyyymmdd_list_from_directory2(data)
 
-    train_per_each_episode(max_secs, max_len, csv, pickles, max_size)
-
-
-def train_per_each_episode(max_secs, max_len, csv, pickles, max_size):
-
     model = build_network(max_secs, max_len)
     model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
     model.summary()
 
     # {steps} --> this file will be saved whenver it runs every steps as much as {step}
-    checkpoint_weights_filename = 'boa_weights_{step}.h5f'
+    checkpoint_weights_filename = 'boa_weights_{step}.h5'
 
     #model.load_weights(filepath = checkpoint_weights_filename.format(step=0), by_name=True, skip_mismatch=True)
 
     # TODO: here we can add hyperparameters information like below!!
     log_filename = 'boa_{}_log.json'.format(max_secs)
-    checkpoint_interval = 50000
+    checkpoint_interval = 50
 
     callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename, interval=checkpoint_interval)]
     callbacks += [FileLogger(log_filename, interval=100)]
 
-    # 전체를 몇 episode 로 할 것인가
-    max_episode_cnt = 5000
-    # num_of_episode = int(max_size / max_episode_cnt)
+    x1, x2, x3, y = get_real_data(max_secs, max_len, csv, pickles)
 
-    for episode in range(0, max_episode_cnt):
-        x1, x2, x3, y = get_real_data(max_secs, max_len, csv, pickles)
+    # print('shape : ', x1.shape, x2.shape, y.shape)
+    # model.fit({'x1': x1, 'x2': x2, 'x3': x3}, y, epochs=10, verbose=2, batch_size=64, callbacks=callbacks)
+    # # model.fit({'x1': x1, 'x2': x2}, y, epochs=10, verbose=2, batch_size=64, callbacks=callbacks)
 
-        print('shape : ', x1.shape, x2.shape, y.shape)
-        model.fit({'x1': x1, 'x2': x2, 'x3': x3}, y, epochs=10, verbose=2, batch_size=64, callbacks=callbacks)
-        # model.fit({'x1': x1, 'x2': x2}, y, epochs=10, verbose=2, batch_size=64, callbacks=callbacks)
 
-        if episode % 50 == 0:
-            # model.save_weights(filepath=checkpoint_weights_filename.format(step=episode))
-            model.save(filepath=checkpoint_weights_filename.format(step=episode))
+    print('start to train.')
+    # model.fit({'x1': t_x1, 'x2': t_x2}, t_y1, epochs=50, verbose=2, batch_size=64, callbacks=callbacks)
+    # model.save_weights('final_weight.h5f')
 
-    # model.save_weights(filepath=checkpoint_weights_filename.format(step='end'))
-    model.save(filepath=checkpoint_weights_filename.format(step='end'))
+    # create model
+    model = KerasRegressor(build_fn=build_network, verbose=0)
+    # define the grid search parameters
+    batch_size = [10, 20, 40, 60, 80, 100]
+    epochs = [10, 50, 100]
+    neurons = [15, 20, 25, 30]
+    param_grid = dict(batch_size=batch_size, epochs=epochs, neurons=neurons)
+
+    grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1)
+    # grid_result = grid.fit({'x1': t_x1, 'x2': t_x2}, t_y1)
+
+    grid_result = grid.fit(
+        np.array(  [ {'x1': a, 'x2': b, 'x3': c}  for a, b, c in zip(x1, x2, x3)]),
+        y)
+
+    # grid_result = grid.fit(
+    #     np.array([{'x1': a, 'x2': b}] for a, b in zip(t_x1, t_x2)),
+    #     t_y1)
+
+
+    # summarize results
+    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+    means = grid_result.cv_results_['mean_test_score']
+    stds = grid_result.cv_results_['std_test_score']
+    params = grid_result.cv_results_['params']
+    for mean, stdev, param in zip(means, stds, params):
+        print("%f (%f) with: %r" % (mean, stdev, param))
+
+
 
 
 def main():
