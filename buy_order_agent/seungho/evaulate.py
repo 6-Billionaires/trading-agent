9cad062c8cc35a007320d214b33e92c1464163c0
Unknown <onmylife82@gmail.com>
2018-09-11 22:48:20 +0900

    평가모델 및 웨이트 저장

--- buy_order_agent/seungho/evaulate.py
+++ buy_order_agent/seungho/evaulate.py
@@ -16,11 +16,10 @@ build q newtork using cnn and dense layer
 """
 
 
-def build_network(max_secs = 90, max_len = 7, optimizer='adam',init_mode='uniform', ):
-
+def build_network(max_secs=90, max_len=7, optimizer='adam', init_mode='uniform', ):
     input_order = Input(shape=(10, 2, max_secs, 2), name="x1")
     input_tranx = Input(shape=(max_secs, 11), name="x2")
-    input_remain_secs = Input(shape=(max_len, ), name="x3") # update. remained seconds up to 180 seconds ??
+    input_remain_secs = Input(shape=(max_len,), name="x3")  # update. remained seconds up to 180 seconds ??
 
     h_conv1d_2 = Conv1D(filters=16, kernel_size=3, activation='relu')(input_tranx)
     h_conv1d_4 = MaxPooling1D(pool_size=3, strides=None, padding='valid')(h_conv1d_2)
@@ -45,7 +44,6 @@ def build_network(max_secs = 90, max_len = 7, optimizer='adam',init_mode='unifor
     i_concatenated_all_h_1 = Flatten()(h_conv1d_8)
 
     i_concatenated_all_h = Concatenate()([i_concatenated_all_h_1, o_conv3d_1_1, input_remain_secs])
-    # i_concatenated_all_h = Concatenate()([i_concatenated_all_h_1, o_conv3d_1_1])
 
     i_concatenated_all_h = Dense(10, activation='linear')(i_concatenated_all_h)
     output = Dense(1, kernel_initializer=init_mode, activation='linear')(i_concatenated_all_h)
@@ -57,7 +55,7 @@ def build_network(max_secs = 90, max_len = 7, optimizer='adam',init_mode='unifor
     return model
 
 
-def get_real_data(max_secs, max_len, csv, pickles, max_stock = 5):
+def get_real_data(max_secs, max_len, csv, pickles, max_stock = 10):
     x1_dimension_info = (10, 2, max_secs, 2)
     x2_dimension_info = (max_secs, 11)
     x3_dimension_info = (max_len,)
@@ -68,10 +66,9 @@ def get_real_data(max_secs, max_len, csv, pickles, max_stock = 5):
     d_y1 = []
 
     keys = list(pickles.keys())
+    keys = keys[::-1]
+    # random.shuffle(keys)
 
-    random.shuffle(keys)
-
-    max_stock = max_stock if max_stock < len(keys) else len(keys)
     stock = 0
     for key in keys:
         stock = stock + 1
@@ -135,57 +132,13 @@ def train_using_real_data(data, max_secs, max_len, temp=True):
 
     model = build_network(max_secs, max_len)
 
-    # {steps} --> this file will be saved whenver it runs every steps as much as {step}
-    checkpoint_weights_filename = 'boa_weights_{step}.h5'
+    # load weight
+    model.load_weights('final_weight.h5')
 
-    #model.load_weights(filepath = checkpoint_weights_filename.format(step=0), by_name=True, skip_mismatch=True)
+    x1, x2, x3, y = get_real_data(max_secs, max_len, csv, pickles, 10)
 
-    # TODO: here we can add hyperparameters information like below!!
-    log_filename = 'boa_{}_log.json'.format(max_secs)
-    checkpoint_interval = 50
-
-    callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename, interval=checkpoint_interval)]
-    callbacks += [FileLogger(log_filename, interval=100)]
-
-    x1, x2, x3, y = get_real_data(max_secs, max_len, csv, pickles, 28)
-
-    if temp:
-        # print('shape : ', x1.shape, x2.shape, y.shape)
-        model.fit({'x1': x1, 'x2': x2, 'x3': x3}, y, epochs=100, verbose=2, batch_size=64, callbacks=callbacks)
-        model.save('final_weight.h5')
-    # else:
-        # print('start to train.')
-        # # model.fit({'x1': t_x1, 'x2': t_x2}, t_y1, epochs=50, verbose=2, batch_size=64, callbacks=callbacks)
-        # # model.save_weights('final_weight.h5f')
-        #
-        # # create model
-        # model = KerasRegressor(build_fn=build_network, verbose=0)
-        # # define the grid search parameters
-        # batch_size = [10, 20, 40, 60, 80, 100]
-        # epochs = [10, 50, 100]
-        # neurons = [15, 20, 25, 30]
-        # param_grid = dict(batch_size=batch_size, epochs=epochs, neurons=neurons)
-        #
-        # grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1)
-        # # grid_result = grid.fit({'x1': t_x1, 'x2': t_x2}, t_y1)
-        #
-        # grid_result = grid.fit(
-        #     np.array(  [ {'x1': a, 'x2': b, 'x3': c}  for a, b, c in zip(x1, x2, x3)]),
-        #     y)
-        #
-        # # grid_result = grid.fit(
-        # #     np.array([{'x1': a, 'x2': b}] for a, b in zip(t_x1, t_x2)),
-        # #     t_y1)
-        #
-        #
-        # # summarize results
-        # print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
-        # means = grid_result.cv_results_['mean_test_score']
-        # stds = grid_result.cv_results_['std_test_score']
-        # params = grid_result.cv_results_['params']
-        # for mean, stdev, param in zip(means, stds, params):
-        #     print("%f (%f) with: %r" % (mean, stdev, param))
-
+    scores = model.evaluate({'x1': x1, 'x2': x2, 'x3': x3}, y, verbose=0)
+    print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
 
 
 
