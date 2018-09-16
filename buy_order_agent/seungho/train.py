3c7586107a7497ab44268ccc4423ddadd7e8ce0b
Seungho <onmylife82@gmail.com>
2018-09-16 20:52:12 +0900

    grid 적용값

--- buy_order_agent/seungho/train.py
+++ buy_order_agent/seungho/train.py
@@ -6,8 +6,8 @@ from rl.callbacks import FileLogger, ModelIntervalCheckpoint
 from gym_core import ioutil  # file i/o to load stock csv files
 import random
 from core import util
-from core.scikit_learn_multi_input_boa import KerasRegressor
-from sklearn.model_selection import GridSearchCV
+# from core.scikit_learn_multi_input_boa import KerasRegressor
+# from sklearn.model_selection import GridSearchCV
 
 """
 build q newtork using cnn and dense layer
@@ -129,7 +129,7 @@ def get_real_data(max_secs, max_len, csv, pickles, max_stock = 5):
     return np.asarray(d_x1), np.asarray(d_x2), np.asarray(d_x3), np.asarray(d_y1)
 
 
-def train_using_real_data(data, max_secs, max_len, isGrid=True):
+def train_using_real_data(data, max_secs, max_len, isGrid=False):
     # {일자 + 종목코드} : {meta, quote, order}
     csv = ioutil.load_data_from_directory2('0')
     # {일자 + 종목코드} : [second, left_time, elapsed_time, y]
@@ -151,36 +151,37 @@ def train_using_real_data(data, max_secs, max_len, isGrid=True):
     x1, x2, x3, y = get_real_data(max_secs, max_len, csv, pickles, 28)
 
     if isGrid == False:
-        model = build_network(max_secs, max_len)
+        # def build_network(optimizer='adam', init_mode='uniform', filters=16, neurons=20, max_secs=90, max_len=7):
+        model = build_network('adam', 'uniform', 16, 20, max_secs, max_len)
         # print('shape : ', x1.shape, x2.shape, y.shape)
-        model.fit({'x1': x1, 'x2': x2, 'x3': x3}, y, epochs=70, verbose=2, batch_size=10, callbacks=callbacks)
+        model.fit({'x1': x1, 'x2': x2, 'x3': x3}, y, epochs=60, verbose=2, batch_size=10, callbacks=callbacks)
         model.save('final_weight.h5')
-    else:
-        print('start to train.')
-        # model.fit({'x1': t_x1, 'x2': t_x2}, t_y1, epochs=50, verbose=2, batch_size=64, callbacks=callbacks)
-        # model.save_weights('final_weight.h5f')
-
-        # create model
-        model = KerasRegressor(build_fn=build_network, verbose=0)
-        # define the grid search parameters
-        batch_size = [10, 20, 40, 60, 80, 100]
-        epochs = [10, 50, 100]
-        neurons = [15, 20, 25, 30]
-        param_grid = dict(batch_size=batch_size, epochs=epochs, neurons=neurons)
-
-        grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1)
-        # grid_result = grid.fit({'x1': t_x1, 'x2': t_x2}, t_y1)
-
-        # grid_result = grid.fit(np.array([{'x1': a, 'x2': b} for a, b in zip(x1, x2)]), y)
-        grid_result = grid.fit(np.array([{'x1': a, 'x2': b, 'x3': c} for a, b, c in zip(x1, x2, x3)]), y)
-
-        # summarize results
-        print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
-        means = grid_result.cv_results_['mean_test_score']
-        stds = grid_result.cv_results_['std_test_score']
-        params = grid_result.cv_results_['params']
-        for mean, stdev, param in zip(means, stds, params):
-            print("%f (%f) with: %r" % (mean, stdev, param))
+    # else:
+    #     print('start to train.')
+    #     # model.fit({'x1': t_x1, 'x2': t_x2}, t_y1, epochs=50, verbose=2, batch_size=64, callbacks=callbacks)
+    #     # model.save_weights('final_weight.h5f')
+    #
+    #     # create model
+    #     model = KerasRegressor(build_fn=build_network, verbose=0)
+    #     # define the grid search parameters
+    #     batch_size = [10, 20, 40, 60, 80, 100]
+    #     epochs = [10, 50, 100]
+    #     neurons = [15, 20, 25, 30]
+    #     param_grid = dict(batch_size=batch_size, epochs=epochs, neurons=neurons)
+    #
+    #     grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1)
+    #     # grid_result = grid.fit({'x1': t_x1, 'x2': t_x2}, t_y1)
+    #
+    #     # grid_result = grid.fit(np.array([{'x1': a, 'x2': b} for a, b in zip(x1, x2)]), y)
+    #     grid_result = grid.fit(np.array([{'x1': a, 'x2': b, 'x3': c} for a, b, c in zip(x1, x2, x3)]), y)
+    #
+    #     # summarize results
+    #     print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+    #     means = grid_result.cv_results_['mean_test_score']
+    #     stds = grid_result.cv_results_['std_test_score']
+    #     params = grid_result.cv_results_['params']
+    #     for mean, stdev, param in zip(means, stds, params):
+    #         print("%f (%f) with: %r" % (mean, stdev, param))
 
 
 
