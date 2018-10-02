3f3b3282cc61595ffafe3c4c0e9b45761c14e182
donilee <ldspisee@gmail.com>
2018-10-02 20:37:37 +0900

    modify metrics

---
+++ sell_signal_agent/ssa_metrics.py
@@ -0,0 +1,19 @@
+
+import keras.backend as K
+
+def mean_pred(y_true, y_pred):
+    return K.mean(y_pred)
+
+def theil_u(y_true, y_pred):
+    up = K.sqrt(K.mean(K.square(y_true - y_pred)))
+    bottom = K.sqrt(K.mean(K.square(y_true))) + K.sqrt(K.mean(K.square(y_pred)))
+    return up / bottom
+
+def r(y_true, y_pred):
+    mean_y_true = K.mean(y_true)
+    mean_y_pred = K.mean(y_pred)
+
+    up = K.sum((y_true - mean_y_true) * (y_pred - mean_y_pred))
+    bottom = K.sqrt(K.sum(K.square(y_true - mean_y_true)) * K.sum(K.square(y_pred - mean_y_pred)))
+
+    return up / bottom
