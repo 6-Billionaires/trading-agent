8ea4c6b67d0e8ae7c9982216f97ac76de99e0789
doni <doni@doniui-MacBook-Pro.local>
2018-10-04 04:51:19 +0900

    result report

--- buy_signal_agent/ds/pickup_result.py
+++ buy_signal_agent/ds/pickup_result.py
@@ -2,11 +2,11 @@
 import pickle
 
 pickle_names = [
-                '/Users/doni/99_study/git/trading-agent/sell_signal_agent/ssa_model_history_e75_b70_n75',
-                '/Users/doni/99_study/git/trading-agent/sell_signal_agent/ssa_model_history_e50_b70_n125',
-                '/Users/doni/99_study/git/trading-agent/sell_signal_agent/ssa_model_history_e100_b30_n175',
-                '/Users/doni/99_study/git/trading-agent/sell_signal_agent/ssa_model_history_e100_b50_n175',
-                '/Users/doni/99_study/git/trading-agent/sell_signal_agent/ssa_model_history_e100_b70_n125'
+                '/Users/doni/99_study/git/trading-agent/buy_signal_agent/ds/bsa_model_history_e70_b10_n100',
+                '/Users/doni/99_study/git/trading-agent/buy_signal_agent/ds/bsa_model_history_e70_b10_n120',
+                '/Users/doni/99_study/git/trading-agent/buy_signal_agent/ds/bsa_model_history_e70_b10_n150',
+                '/Users/doni/99_study/git/trading-agent/buy_signal_agent/ds/bsa_model_history_e70_b20_n100',
+                '/Users/doni/99_study/git/trading-agent/buy_signal_agent/ds/bsa_model_history_e100_b10_n70'
                 ]
 
 for pickle_name in pickle_names:
